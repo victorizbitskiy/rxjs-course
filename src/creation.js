8b7bd8a50adda62395f0a8119ae378c4f572a99f
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-10 21:48:43 +0300

    stream from

--- src/creation.js
+++ src/creation.js
@@ -1,7 +1,14 @@
-import { of } from 'rxjs'
+import { of, from } from 'rxjs'
+import { scan } from 'rxjs/operators'
 
-const stream$ = of(1, 2, 3, 4)
+// const stream$ = of(1, 2, 3, 4)
 
-stream$.subscribe(val => {
-  console.log('Value:', val)
-})
+// stream$.subscribe(val => {
+//   console.log('Value:', val)
+// })
+
+// const array$ = from([1, 2, 3, 4]).pipe(
+//   scan((acc, v) => acc.concat(v), [])
+// )
+
+// array$.subscribe(val => console.log(val))
