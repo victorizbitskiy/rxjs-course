6b0bbe0c3ca7b4e9ef7ce669f179b911695a6402
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-12 18:09:04 +0300

    fromEvent

--- src/creation.js
+++ src/creation.js
@@ -1,4 +1,4 @@
-import { of, from, Observable } from 'rxjs'
+import { of, from, Observable, fromEvent, map } from 'rxjs'
 import { observeOn } from 'rxjs-compat/operator/observeOn'
 import { scan } from 'rxjs/operators'
 
@@ -14,13 +14,13 @@ import { scan } from 'rxjs/operators'
 
 // array$.subscribe(val => console.log(val))
 
-const stream$ = new Observable(observer => {
-  observer.next('First value')
-  setTimeout(() => observer.next('After 1000 ms'), 1000);
-  setTimeout(() => observer.complete(), 1500);
-  // setTimeout(() => observer.error('Something went wrong'), 1000);
-  setTimeout(() => observer.next('After 3000 ms'), 3000);
-})
+// const stream$ = new Observable(observer => {
+//   observer.next('First value')
+//   setTimeout(() => observer.next('After 1000 ms'), 1000);
+//   setTimeout(() => observer.complete(), 1500);
+//   // setTimeout(() => observer.error('Something went wrong'), 1000);
+//   setTimeout(() => observer.next('After 3000 ms'), 3000);
+// })
 
 // stream$.subscribe(
 //   (val) => console.log('Val:', val),
@@ -40,3 +40,22 @@ const stream$ = new Observable(observer => {
 //     console.log()
 //   }
 // })
+
+fromEvent(document.querySelector('canvas'), 'mousemove')
+  .pipe(
+    map(e => ({
+      x: e.offsetX,
+      y: e.offsetY,
+      ctx: e.target.getContext('2d')
+    }))
+  )
+  .subscribe(pos => {
+    pos.ctx.fillRect(pos.x, pos.y, 2, 2)
+  })
+
+const clear$ = fromEvent(document.getElementById('clear'), 'click')
+
+clear$.subscribe(() => {
+  const canvas = document.querySelector('canvas')
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+})
