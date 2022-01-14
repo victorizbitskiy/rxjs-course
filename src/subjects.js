cfb81880e22937ba2bac1b58d58e852ae4f6faae
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-14 18:10:09 +0300

    ReplaySubject

--- src/subjects.js
+++ src/subjects.js
@@ -1,4 +1,4 @@
-import { Subject, BehaviorSubject } from 'rxjs'
+import { Subject, BehaviorSubject, ReplaySubject} from 'rxjs'
 
 // document.addEventListener('click', () => {
 //   const stream$ = new Subject()
@@ -8,10 +8,21 @@ import { Subject, BehaviorSubject } from 'rxjs'
 //   stream$.next('JS')
 // })
 
+// document.addEventListener('click', () => {
+//   const stream$ = new BehaviorSubject('First')
+//   stream$.subscribe(v => console.log('Value:', v))
+//   stream$.next('Hello')
+//   stream$.next('Rx')
+//   stream$.next('JS')
+// })
+
 document.addEventListener('click', () => {
-    const stream$ = new BehaviorSubject('First')
-    stream$.subscribe(v => console.log('Value:', v))
-    stream$.next('Hello')
-    stream$.next('Rx')
-    stream$.next('JS')
-  })
+  const stream$ = new ReplaySubject(2)
+
+  stream$.next('Hello')
+  stream$.next('Rx')
+  stream$.next('JS')
+
+  // Сохраняет предыдущие значения, которые мы диспачили до подписки
+  stream$.subscribe(v => console.log('Value:', v))
+})
