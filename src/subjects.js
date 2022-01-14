819a329578912c2fa4af1a97320726359f999127
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-14 18:07:47 +0300

    BehaviorSubject

--- src/subjects.js
+++ src/subjects.js
@@ -1,9 +1,17 @@
-import { Subject } from 'rxjs'
+import { Subject, BehaviorSubject } from 'rxjs'
+
+// document.addEventListener('click', () => {
+//   const stream$ = new Subject()
+//   stream$.subscribe(v => console.log('Value:', v))
+//   stream$.next('Hello')
+//   stream$.next('Rx')
+//   stream$.next('JS')
+// })
 
 document.addEventListener('click', () => {
-  const stream$ = new Subject()
-  stream$.subscribe(v => console.log('Value', v))
-  stream$.next('Hello')
-  stream$.next('Rx')
-  stream$.next('JS')
-})
+    const stream$ = new BehaviorSubject('First')
+    stream$.subscribe(v => console.log('Value:', v))
+    stream$.next('Hello')
+    stream$.next('Rx')
+    stream$.next('JS')
+  })
