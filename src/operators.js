0ec204efb004a3d149bae0750697159eacb05524
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-17 21:14:39 +0300

    scan, reduce

--- src/operators.js
+++ src/operators.js
@@ -1,14 +1,19 @@
 import { interval } from 'rxjs'
-import { map, filter, tap, take, takeLast, takeWhile } from 'rxjs/operators'
+import { map, filter, tap, take, takeLast, takeWhile, scan, reduce } from 'rxjs/operators'
 
 const stream$ = interval(1000)
   .pipe(
-    tap(v => console.log('Tap:', v)),
+    // tap(v => console.log('Tap:', v)),
+    
+    take(10),  // для проверки reduce()
+
     // map(v => v * 2),
     // filter(v => v % 2 === 0),
     // take(10),
     // takeLast(5)
-    takeWhile(v => v < 7)
+    // takeWhile(v => v < 7)
+    // scan((acc, v) => acc + v, 0)
+    reduce((acc, v) => acc + v, 0),
   )
 
 stream$.subscribe({
