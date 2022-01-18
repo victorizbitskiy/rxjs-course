a73e38731ac63a8a66977ca910f9504cdfa14e64
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-01-18 14:59:45 +0300

    switchMap

--- src/operators.js
+++ src/operators.js
@@ -1,18 +1,20 @@
 import { interval, fromEvent } from 'rxjs'
-import { map, filter, tap, take, takeLast, takeWhile, scan, reduce } from 'rxjs/operators'
+import { map, filter, tap, take, takeLast, takeWhile, scan, reduce, switchMap } from 'rxjs/operators'
 
 fromEvent(document, 'click')
-  .subscribe(() => {
-    const stream$ = interval(1000)
-      .pipe(
-        tap(v => console.log('Tap:', v)),
-        take(5),
-        reduce((acc, v) => acc + v, 0))
-
-    stream$.subscribe({
-      next: v => console.log('Next:', v),
-      complete: () => console.log('Complete')
+  .pipe(
+    switchMap(event => {
+      return interval(1000)
+        .pipe(
+          tap(v => console.log('Tap:', v)),
+          take(5),
+          reduce((acc, v) => acc + v, 0)
+        )
     })
+  )
+  .subscribe({
+    next: v => console.log('Next:', v),
+    complete: () => console.log('Complete')
   })
 
 // const stream$ = interval(1000)
